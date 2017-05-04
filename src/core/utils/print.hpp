ce64c80203a07ba1d1872f086cfe24d14136e7f7
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-05-04 14:01:38 +0200

    utils: Python-like print function.

---
+++ src/core/utils/print.hpp
@@ -0,0 +1,23 @@
+#ifndef CORE_UTILS_PRINT_HPP
+#define CORE_UTILS_PRINT_HPP
+
+#include <iostream>
+
+namespace Utils {
+
+/**
+ * @brief Python style print function.
+ */
+template <typename T> void print(T v) { std::cout << v << '\n'; }
+
+/**
+ * @brief Python style print function.
+ */
+template <typename Arg, typename... Args> void print(Arg v, Args... args) {
+  std::cout << v << " ";
+  print(args...);
+}
+
+} /* namespace Utils */
+
+#endif
