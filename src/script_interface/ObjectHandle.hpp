37f5ad826ab391461750cdcfbc5a48d8ef55e0c9
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-05-20 21:42:58 +0200

    script_interface: Formatting

--- src/script_interface/ObjectHandle.hpp
+++ src/script_interface/ObjectHandle.hpp
@@ -24,15 +24,12 @@
 #include "Variant.hpp"
 
 #include <utils/Span.hpp>
-#include <utils/print.hpp>
 
-#include <boost/mpi/collectives.hpp>
-#include <boost/serialization/utility.hpp>
 #include <boost/utility/string_ref.hpp>
+#include <boost/optional.hpp>
 
 #include <map>
 #include <memory>
-#include <type_traits>
 
 namespace ScriptInterface {
 namespace detail {
